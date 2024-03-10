25d9a89ac61841fc48f4081adbef28c65bf3d186
VeniaminIllarionov <venya957@gmail.com>
2024-03-10 13:07:10 +0300

    'add poetry'

--- main.py
+++ main.py
@@ -2,16 +2,21 @@ from src.class_vacansy import Vacansy
 
 
 def main():
-    user_name = input("Введите пожалуйста, вакансию которую хотели бы найти")
+    print("Введите пожалуйста, вакансию которую хотели бы найти")
+    user_name = input().title()
     while True:
-        user_salary = int(input("Введите пожалуйста, зарплату которую хотели бы получать"))
+        print("Введите пожалуйста, зарплату которую хотели бы получать")
+        user_salary = int(input())
         if type(user_salary) is int:
             break
+        print("Попробуйте еще!")
 
     while True:
-        user_city = input("Введите пожалуйста, город где хотели бы найти работу").title()
+        print(("Введите пожалуйста, город где хотели бы найти работу"))
+        user_city = input().title()
         if type(user_city) is str:
             break
+        print("Попробуйте еще!")
     user = Vacansy(name=user_name, salary=user_salary, city=user_city)
     user.vacansy()
     user.top_salary
